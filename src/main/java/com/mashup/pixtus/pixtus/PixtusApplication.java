0ca06dbdf00aa08aee5b120841c3cdb900364143
sdh2398 <ehdgus2398@gmail.com>
2019-06-29 16:42:06 +0900

    import project

---
+++ src/main/java/com/mashup/pixtus/pixtus/PixtusApplication.java
@@ -0,0 +1,13 @@
+package com.mashup.pixtus.pixtus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixtusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixtusApplication.class, args);
+	}
+
+}
