e8f798df4f05a59977a84c0687b3e1d513986649
sdh2398 <ehdgus2398@gmail.com>
2019-07-22 01:54:12 +0900

    application.yml 추가

--- src/main/java/com/mashup/pixtus/pixtus/jwt/JwtService.java
+++ src/main/java/com/mashup/pixtus/pixtus/jwt/JwtService.java
@@ -5,6 +5,7 @@ import java.security.Key;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
@@ -22,7 +23,8 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 public class JwtService {
 	
-	private final String secretKey = "";
+	@Value("${jwt.secret-key}")
+	private String secretKey;
 	
 	private final static int EXPIRE_TIME_MILLIS = 20000;
 
